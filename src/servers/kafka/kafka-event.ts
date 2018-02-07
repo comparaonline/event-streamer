0748a34cb069b520e155705a81e7ef28aaef8d46
Ezequiel Rabinovich <erabinovich@gmail.com>
2018-02-07 18:00:21 -0300

    Allow changing key and topic of kafka events (#6)

--- src/servers/kafka/kafka-event.ts
+++ src/servers/kafka/kafka-event.ts
@@ -1,6 +1,6 @@
 import { BaseEvent } from '../../event/index';
 
 export abstract class KafkaEvent extends BaseEvent {
-  readonly key?: string;
-  readonly topic?: string;
+  key?: string;
+  topic?: string;
 }
