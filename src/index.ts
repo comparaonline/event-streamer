7c3ba8bbe8b82c23644724d7bc6dd80c1d96b9f3
Jacob Parra <jparra@comparaonline.com>
2018-04-04 20:12:17 -0300

    Fixes main exports

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@
 export { Router } from './router';
 export { InputEvent, OutputEvent } from './events';
 export { Action } from './action';
-export { Server } from './server';
 export { KafkaEvent } from './kafka/kafka-event';
-export { KafkaServer } from './kafka';
+export { KafkaServer } from './kafka/kafka-server';
 
 // Test helpers
 export { TestServer } from './test-helpers';
