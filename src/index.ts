692752799fb24a4415542645e7c298f1af82db3f
Federico Martinez <federico.emanuel.martinez@gmail.com>
2018-06-19 16:40:30 -0400

    Export kafka classes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { Router } from './router';
 export { InputEvent, OutputEvent } from './events';
 export { Action } from './action';
 export { Server } from './server';
-
+export { KafkaServer, KafkaInputEvent, KafkaOutputEvent } from './kafka';
 // Test helpers
 export { TestServer } from './test-helpers';
