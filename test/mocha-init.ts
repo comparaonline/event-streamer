288e317af1f14e7511088bb187a08a55ab95ece7
Jacob Parra <jparra@comparaonline.com>
2018-07-16 21:55:18 -0400

    Changes mocha as test runner

---
+++ test/mocha-init.ts
@@ -0,0 +1,7 @@
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+// Register chai plugins here
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
