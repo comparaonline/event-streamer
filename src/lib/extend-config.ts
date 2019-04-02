07ff531f9bdedd14c950faf8deafbe42fbb6bdf6
Ezequiel Rabinovich <erabinovich@gmail.com>
2019-04-02 18:28:57 -0300

    Fix an issue with latest node-config version

--- src/lib/extend-config.ts
+++ src/lib/extend-config.ts
@@ -1,4 +1,5 @@
 process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
+process.env.ALLOW_CONFIG_MUTATIONS = 'y';
 import config = require('config');
 
 export const extendConfig = <A>(configs: Partial<A>, defaultConfig: Partial<A>): A => {
