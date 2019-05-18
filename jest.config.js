efa06983f38540fd8ac29de742358d826eeb97ef
Ezequiel Rabinovich <erabinovich@gmail.com>
2019-05-18 01:08:10 -0400

    Require 100% coverage

--- jest.config.js
+++ jest.config.js
@@ -18,10 +18,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0
     }
   },
 
