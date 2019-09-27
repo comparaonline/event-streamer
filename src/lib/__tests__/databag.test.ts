b0f9bf7ae565fc0b98e4c0c827aa681aa462b4fc
Ezequiel Rabinovich <erabinovich@gmail.com>
2019-09-27 16:24:29 -0300

    feat: add databag to simplify carrying extra data on observables

---
+++ src/lib/__tests__/databag.test.ts
@@ -0,0 +1,156 @@
+import { from, of } from 'rxjs';
+import { Databag } from '../databag';
+import { toArray, map, concatMap } from 'rxjs/operators';
+
+describe('Databag', () => {
+  describe('#wrap', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues);
+    it('does not change the amount of data in the observable', async () => {
+      const result = await obs.pipe(Databag.wrap(), toArray()).toPromise();
+      expect(result).toHaveLength(testValues.length);
+    });
+
+    it('wraps around the provided data', async () => {
+      const results = await obs.pipe(Databag.wrap(), toArray()).toPromise();
+      results.forEach((result, idx) =>
+        expect(result).toHaveProperty('data', testValues[idx])
+      );
+    });
+  });
+
+  describe('#unwrap', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues).pipe(Databag.wrap());
+    it('does not change the amount of data in the observable', async () => {
+      const result = await obs.pipe(Databag.unwrap(), toArray()).toPromise();
+      expect(result).toHaveLength(testValues.length);
+    });
+
+    it('unwraps the wrapped data', async () => {
+      const results = await obs.pipe(Databag.unwrap(), toArray()).toPromise();
+      expect(results).toEqual(testValues);
+    });
+  });
+
+  describe('#rewrap', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues);
+    it('rewraps a value using the additional data from another bag', async () => {
+      const bag = await of(null).pipe(Databag.wrap()).toPromise();
+      bag.set('value', 'test');
+      const results = await obs.pipe(
+        Databag.rewrap(bag),
+        toArray()
+      ).toPromise();
+      results.forEach(result => expect(result.get('value')).toEqual('test'));
+    });
+  });
+
+  describe('#inside', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues).pipe(Databag.wrap());
+    it('executes the operator and rewraps the data', async () => {
+      const results = await obs.pipe(
+        Databag.inside(map(val => val + 1)),
+        Databag.unwrap(),
+        toArray()
+      ).toPromise();
+      expect(results).toEqual([2, 3, 4, 5]);
+    });
+
+    it('executes async operators and rewraps the data', async () => {
+      const results = await obs.pipe(
+        Databag.inside(concatMap(val => Promise.resolve(val + 1))),
+        Databag.unwrap(),
+        toArray()
+      ).toPromise();
+      expect(results).toEqual([2, 3, 4, 5]);
+    });
+
+    it('keeps additional data after executing the operator', async () => {
+      const results = await obs.pipe(
+        map(val => val.set('value', 'test')),
+        Databag.inside(concatMap(val => Promise.resolve(val + 1))),
+        toArray()
+      ).toPromise();
+      results.forEach(result => expect(result.get('value')).toEqual('test'));
+    });
+  });
+
+  describe('#insideWithBag', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues).pipe(Databag.wrap(), Databag.set('value', 1));
+    it('executes the operator and rewraps the data', async () => {
+      const results = await obs.pipe(
+        Databag.insideWithBag(bag => map(val => val + bag.get<number>('value'))),
+        Databag.unwrap(),
+        toArray()
+      ).toPromise();
+      expect(results).toEqual([2, 3, 4, 5]);
+    });
+
+    it('executes async operators and rewraps the data', async () => {
+      const results = await obs.pipe(
+        Databag.insideWithBag(bag =>
+          concatMap(val => Promise.resolve(val + bag.get<number>('value')))),
+        Databag.unwrap(),
+        toArray()
+      ).toPromise();
+      expect(results).toEqual([2, 3, 4, 5]);
+    });
+
+    it('keeps additional data after executing the operator', async () => {
+      const results = await obs.pipe(
+        map(val => val.set('value', 'test')),
+        Databag.insideWithBag(bag =>
+          concatMap(val => Promise.resolve(val + bag.get<number>('value')))),
+        toArray()
+      ).toPromise();
+      results.forEach(result => expect(result.get('value')).toEqual('test'));
+    });
+  });
+
+  describe('#set', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues).pipe(Databag.wrap());
+    it('sets a property inside a bag', async () => {
+      const results = await obs.pipe(
+        Databag.set('value', 'test'),
+        toArray()
+      ).toPromise();
+      results.forEach(result => expect(result.get('value')).toEqual('test'));
+    });
+  });
+
+  describe('#setMany', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues).pipe(Databag.wrap());
+    it('sets a property inside a bag', async () => {
+      const results = await obs.pipe(
+        Databag.setMany({
+          value: 'test',
+          value2: 'test2'
+        }),
+        toArray()
+      ).toPromise();
+      results.forEach((result) => {
+        expect(result.get('value')).toEqual('test');
+        expect(result.get('value2')).toEqual('test2');
+      });
+    });
+  });
+
+  describe('#get', () => {
+    const testValues = [1, 2, 3, 4];
+    const obs = from(testValues).pipe(Databag.wrap());
+    it('maps to a property inside a bag', async () => {
+      const results = await obs.pipe(
+        Databag.set('value', 'test'),
+        Databag.get('value'),
+        toArray()
+      ).toPromise();
+      expect(results).toEqual(testValues.map(() => 'test'));
+    });
+  });
+});
---
+++ src/lib/databag.ts
@@ -0,0 +1,67 @@
+import { OperatorFunction, Observable, of } from 'rxjs';
+import { map, flatMap } from 'rxjs/operators';
+
+type DatabagOperator<A, B> = OperatorFunction<Databag<A>, Databag<B>>;
+type DynamicOperator<A, B> = (db: Databag<A>) => OperatorFunction<A, B>;
+
+export class Databag<T> {
+  private additional = {};
+  private constructor(private data: T) {}
+
+  set(name: string, value: any) {
+    this.additional[name] = value;
+    return this;
+  }
+
+  setMany(obj: { [k: string]: any }) {
+    this.additional = {
+      ...this.additional,
+      ...obj
+    };
+    return this;
+  }
+
+  get<A>(name: string): A {
+    return this.additional[name];
+  }
+
+  static wrap<A>(): OperatorFunction<A, Databag<A>> {
+    return (obs: Observable<A>) => obs.pipe(map(val => new Databag(val)));
+  }
+
+  static unwrap<A>(): OperatorFunction<Databag<A>, A> {
+    return (obs: Observable<Databag<A>>) => obs.pipe(map(val => val.data));
+  }
+
+  static rewrap<A, B>(bag: Databag<A>): OperatorFunction<B, Databag<B>> {
+    return map((value: B) => {
+      const newBag = new Databag(value);
+      newBag.additional = bag.additional;
+      return newBag;
+    });
+  }
+
+  static inside<A, B>(op: OperatorFunction<A, B>): DatabagOperator<A, B> {
+    return (obs: Observable<Databag<A>>) => obs.pipe(
+      flatMap((bag: Databag<A>) => of(bag.data).pipe(op, Databag.rewrap(bag)))
+    );
+  }
+
+  static insideWithBag<A, B>(op: DynamicOperator<A, B>): DatabagOperator<A, B> {
+    return (obs: Observable<Databag<A>>) => obs.pipe(
+      flatMap((bag: Databag<A>) => of(bag.data).pipe(op(bag), Databag.rewrap(bag)))
+    );
+  }
+
+  static set<A>(name: string, value: any): DatabagOperator<A, A> {
+    return map(databag => databag.set(name, value));
+  }
+
+  static setMany<A>(obj: { [k: string]: any }): DatabagOperator<A, A> {
+    return map(databag => databag.setMany(obj));
+  }
+
+  static get<A, B = unknown>(name: string): OperatorFunction<Databag<B>, A> {
+    return map(databag => databag.get<A>(name));
+  }
+}
