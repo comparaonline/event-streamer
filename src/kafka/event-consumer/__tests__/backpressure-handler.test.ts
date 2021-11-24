c854d57e300a3a2607bcb5995b8eda2c376d409f
Oswald Rijo <oswald.rijo@gmail.com>
2021-11-24 12:48:10 -0300

    fix: improve on backpressure

--- src/kafka/event-consumer/__tests__/backpressure-handler.test.ts
+++ src/kafka/event-consumer/__tests__/backpressure-handler.test.ts
@@ -1,21 +1,5 @@
 import { of, MonoTypeOperatorFunction, Subscription } from 'rxjs';
-import { BackpressureHandler, MemoryAction } from '../backpressure-handler';
-import { EventsEnum } from '../../notifier/events-enum';
-
-jest.useFakeTimers();
-
-const mockEmit = jest.fn();
-const mockMemoryUsage = jest.fn();
-
-jest.mock('../../notifier', () => ({
-  Notifier: class {
-    static getInstance() : any {
-      return {
-        emit: mockEmit
-      };
-    }
-  }
-}));
+import { BackpressureHandler } from '../backpressure-handler';
 
 describe('BackpressureHandler', () => {
   let handler: BackpressureHandler;
@@ -29,27 +13,8 @@ describe('BackpressureHandler', () => {
   beforeEach(() => stream = { resume: jest.fn(), pause: jest.fn() });
 
   it('constructor backpressure', () => {
-    // arrange
-    process.memoryUsage = mockMemoryUsage;
-    mockMemoryUsage.mockImplementation(() => ({
-      heapUsed: 1,
-      heapTotal: 2
-    }));
-
     // act
     new BackpressureHandler(stream, 10, 0);
-
-    // assert
-    expect(mockEmit).toHaveBeenNthCalledWith(
-      1,
-      EventsEnum.ON_MEMORY_USED,
-      { action: MemoryAction.heapTotal, memUsed: 2 }
-    );
-    expect(mockEmit).toHaveBeenNthCalledWith(
-      2,
-      EventsEnum.ON_MEMORY_USED,
-      { action: MemoryAction.initial, memUsed: 1 }
-    );
   });
 
   it('keeps track of backpressure', async () => {
@@ -76,30 +41,11 @@ describe('BackpressureHandler', () => {
     afterEach(() => subscription.unsubscribe());
 
     it('stops when it gets to the pause limit', async () => {
-      const ONE_HUNDRED = 100;
-      mockMemoryUsage.mockImplementation(() => ({
-        heapUsed: 1,
-        heapTotal: 2,
-        rss: ONE_HUNDRED
-      }));
-
-      process.memoryUsage = mockMemoryUsage;
-
       await testTap(handler.increment());
       await testTap(handler.increment());
       await testTap(handler.increment());
       expect(stream.pause).toHaveBeenCalledTimes(1);
       expect(stream.resume).not.toHaveBeenCalled();
-      expect(mockEmit).toHaveBeenCalledTimes(6);
-      [
-        { action: MemoryAction.heapTotal, memUsed: 2 },
-        { action: MemoryAction.initial, memUsed: 1 },
-        { action: MemoryAction.rss, memUsed: ONE_HUNDRED }
-      ].forEach((event, index) => expect(mockEmit).toHaveBeenNthCalledWith(
-        index + 1,
-        EventsEnum.ON_MEMORY_USED,
-        event
-      ));
     });
 
     it('stops calling pause when over the limit', async () => {
@@ -112,13 +58,6 @@ describe('BackpressureHandler', () => {
     });
 
     it('calls resume when it goes below the limit after calling pause', async () => {
-      const ONE_HUNDRED = 100;
-      process.memoryUsage = mockMemoryUsage;
-      mockMemoryUsage.mockImplementation(() => ({
-        heapUsed: 1,
-        heapTotal: 2,
-        rss: ONE_HUNDRED
-      }));
       await testTap(handler.increment());
       await testTap(handler.increment());
       await testTap(handler.increment());
@@ -134,39 +73,6 @@ describe('BackpressureHandler', () => {
       await testTap(handler.decrement());
       expect(stream.pause).toHaveBeenCalledTimes(1);
       expect(stream.resume).toHaveBeenCalledTimes(1);
-      expect(mockEmit).toHaveBeenCalledTimes(13);
-      expect(mockEmit).toHaveBeenNthCalledWith(
-        13,
-        EventsEnum.ON_MEMORY_USED,
-        { action: MemoryAction.resumed, memUsed: ONE_HUNDRED }
-      );
-    });
-  });
-  describe('checkMem', () => {
-    it('should emit actions', () => {
-      // arrange
-      process.memoryUsage = mockMemoryUsage;
-      mockMemoryUsage.mockImplementation(() => ({
-        heapUsed: 1,
-        rss: 3,
-        heapTotal: 2
-      }));
-
-      // act
-      handler.checkMem();
-
-      // assert
-      expect(mockEmit).toHaveBeenCalledTimes(3);
-      [
-        { action: MemoryAction.check, memUsed: 1 },
-        { action: MemoryAction.rss, memUsed: 3 },
-        { action: MemoryAction.heapTotal, memUsed: 2 }
-      ].forEach((event, index) => expect(mockEmit).toHaveBeenNthCalledWith(
-        index + 1,
-        EventsEnum.ON_MEMORY_USED,
-        event
-      ));
-
     });
   });
 });
--- src/kafka/event-consumer/backpressure-handler.ts
+++ src/kafka/event-consumer/backpressure-handler.ts
@@ -1,8 +1,5 @@
 import { Subject, of, EMPTY, MonoTypeOperatorFunction } from 'rxjs';
 import { scan, share, tap, distinctUntilChanged, skip, flatMap } from 'rxjs/operators';
-import { EventsEnum } from '../notifier/events-enum';
-import { Notifier } from '../notifier';
-import { defaultLogger } from '../../lib/default-logger';
 
 interface PausableStream {
   pause(): unknown;
@@ -34,16 +31,11 @@ const actions = (stream: PausableStream) => ({
   [Action.pause]: of(() => stream.pause()),
   [Action.resume]: of(() => stream.resume())
 });
-const MB = 1000000;
-const FOUR_HUNDRED = 400;
-const HALF_SEC = 500;
 
 export class BackpressureHandler {
   current = 0;
-  minMemUsage = 0;
   private actions = actions(this.pausableStream);
   private readonly backpressureSubject = new Subject<number>();
-  private notifier = Notifier.getInstance();
 
   public readonly backpressure = this.backpressureSubject.pipe(
     scan((acc, value) => acc + value, 0),
@@ -55,37 +47,8 @@ export class BackpressureHandler {
       /* istanbul ignore next */
       private pause: number = Infinity,
       /* istanbul ignore next */
-      private resume: number = Infinity,
-      /* istanbul ignore next */
-      private topMB: number = FOUR_HUNDRED
-   ) {
-    this.minMemUsage = process.memoryUsage().heapUsed;
-    this.emitMemoryUsage(MemoryAction.heapTotal, process.memoryUsage().heapTotal);
-    this.emitMemoryUsage(MemoryAction.initial, this.minMemUsage);
-    /* istanbul ignore next */
-    setInterval(this.checkMem, HALF_SEC);
-  }
-
-  checkMem = () => {
-    const heap = process.memoryUsage().heapUsed;
-    const rss = process.memoryUsage().rss;
-    const heapTotal = process.memoryUsage().heapTotal;
-
-    this.emitMemoryUsage(MemoryAction.check, heap);
-    this.emitMemoryUsage(MemoryAction.rss, rss);
-    this.emitMemoryUsage(MemoryAction.heapTotal, heapTotal);
-  }
-
-  private emitMemoryUsage (action: MemoryAction, memUsed: number) {
-    try {
-      this.notifier.emit(
-        EventsEnum.ON_MEMORY_USED,
-        <MemoryMetrics>{ action, memUsed });
-    } catch (e) {
-      /* istanbul ignore next */
-      defaultLogger.error(`emitMemoryUsage error sending metrics ${e}`);
-    }
-  }
+      private resume: number = Infinity
+   ) {}
 
   private decrementCurrent() {
     this.current = this.current - 1;
@@ -119,39 +82,19 @@ export class BackpressureHandler {
   }
 
   private chooseAction(prev: Action, current: number) {
-    const rss = process.memoryUsage().rss;
-    this.emitMemoryUsage(MemoryAction.rss, rss);
-
-    const shouldPause = this.shouldPauseConsumer(current, rss);
-    const shouldResume = this.shouldResumeConsumer(prev, current, rss);
+    const shouldPause = this.shouldPauseConsumer(current);
+    const shouldResume = this.shouldResumeConsumer(prev, current);
 
     return shouldPause ? Action.pause : (
       shouldResume ? Action.resume : prev
     );
   }
 
-  private shouldPauseConsumer(current: number, rss: number): boolean {
-    const shouldPause = current >= this.pause || this.hasReachedSuperiorMemoryLimit(rss);
-
-    if (shouldPause) {
-      this.emitMemoryUsage(MemoryAction.paused, rss);
-    }
-
-    return shouldPause;
+  private shouldPauseConsumer(current: number): boolean {
+    return current >= this.pause;
   }
 
-  private shouldResumeConsumer(prev: Action, current: number, rss: number): boolean {
-    const shouldResume = prev === Action.pause &&
-      current <= this.resume;
-
-    if (shouldResume) {
-      this.emitMemoryUsage(MemoryAction.resumed, rss);
-    }
-
-    return shouldResume;
-  }
-
-  private hasReachedSuperiorMemoryLimit(rss: number) {
-    return rss > this.topMB * MB;
+  private shouldResumeConsumer(prev: Action, current: number): boolean {
+    return  prev === Action.pause && current <= this.resume;
   }
 }
