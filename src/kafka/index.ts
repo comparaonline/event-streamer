915a5849da1babadc5289cc7a9d277ffad54c482
Oswald Rijo <oswald.rijo@gmail.com>
2021-11-24 12:56:05 -0300

    fix: delete wrong imports

--- src/kafka/index.ts
+++ src/kafka/index.ts
@@ -2,5 +2,3 @@
 export { KafkaInputEvent, KafkaOutputEvent } from './kafka-events';
 export { KafkaServer } from './kafka-server';
 export { MemoryAction } from './event-consumer/backpressure-handler';
-export { Notifier } from './notifier';
-export { EventsEnum } from './notifier/events-enum';
